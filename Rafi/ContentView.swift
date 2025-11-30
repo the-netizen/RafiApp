f57add9bb45bd5f49ec595782449a4a4ef6bf435
the-netizen <khanaima020815@gmail.com>
2025-11-30 17:48:08 +0300

    Initial Commit

---
+++ Rafi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rafi
+//
+//  Created by Naima Khan on 30/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rafi/RafiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RafiApp.swift
+//  Rafi
+//
+//  Created by Naima Khan on 30/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct RafiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
